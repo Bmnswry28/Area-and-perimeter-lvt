40021306c790e53ebc25051700258dd0fa0bd51d
abbas <bmnswry27@gmail.com>
2024-09-06 21:18:43 +0330

    add js

---
+++ app.js
@@ -0,0 +1,27 @@
+//Rectangle
+length=5;
+y=3;
+rectangle_area = length * y;
+rectangle_perimeter = 2 *(length+y);
+console.log("Rectangel:",rectangle_area,rectangle_perimeter)
+
+//Circle
+
+radius=3;
+circle_area = 3.14 * (radius,2);
+circle_circumference = 2 * 3.14 * radius;
+console.log("Circle:",circle_area,circle_circumference)
+//tringle
+
+base=5;
+height=3;
+triangle_area = 0.5 * base * height;
+triangle_perimeter = base + base + height;
+console.log("Triangle:",triangle_area,triangle_perimeter)
+
+//Square
+
+side=5;
+square_area = Math.pow(side,2);
+square_perimeter = 4 * side;
+console.log("Square:",square_area,square_perimeter)
